761d0609bf76f6b31a18c645911a0e5fe3131b39
Ghin_MLo7 <mashini.ruroii@gmail.com>
2026-09-16 12:41:43 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("cupcakke.ico");
-    res.compile().unwrap();
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
 }
